3257e42c7307a0e4f6de3613d16ab5266c0d35fb
jonb <jonb@lanl.gov>
2012-01-12 11:28:12 -0700

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kgrok::Application.config.secret_token = '925a33d3ad44f83578ebe0379223102882c59944f9d04aa8e859ff16eeeb68d441d86e33be32191430439d20804155319a4eafcf8e051eabf17e42c899254092'
